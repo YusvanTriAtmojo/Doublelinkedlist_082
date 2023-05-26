98d7ecacdd9670ec19bedacb1c49e94d6f482864
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2023-05-26 15:47:44 +0700

    Membuat method addNode, search, deleteNode, listempty, traverse, revtraverse, hapus dan searchData di dalam class DoubleLinkedList dengan access public

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -13,4 +13,14 @@ public:
 class DoubleLinkedList {
 private:
 	Node* START = NULL;
+public:
+
+	void addNode();
+	bool search(int rollNo, Node** previous, Node** current);
+	bool deleteNode(int rollNo);
+	bool listEmpty();
+	void traverse();
+	void revtraverse();
+	void hapus();
+	void searchData();
 };
