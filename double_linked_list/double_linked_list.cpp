7f9dd994baee7e4be596935b6f18c646804e9f6f
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2023-05-26 18:55:27 +0700

    Mengisi method searchData dari class DoubleLinkedList untuk memasukkan number yang dicari kemudian menampilkan keterangan berdasarkan method search

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -144,3 +144,21 @@ void DoubleLinkedList::hapus() {
 	else
 		cout << "Record with roll number " << rollNo << "delete" << endl;
 }
+void DoubleLinkedList::searchData() {
+	if (listEmpty() == true) {
+		cout << "\List is empty" << endl;
+
+	}
+	Node* prev, * curr;
+	prev = curr = NULL;
+	cout << "\nEnter the roll number of the student whose record you want to search: ";
+	int num;
+	cin >> num;
+	if (DoubleLinkedList::search(num, &prev, &curr) == false)
+		cout << "\nRecord not found" << endl;
+	else {
+		cout << "\nRecord found" << endl;
+		cout << "\nRoll number: " << endl;
+		cout << "\nName: " << endl;
+	}
+}
